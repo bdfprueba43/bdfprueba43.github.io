76dbfb5f20e3cd68ee639f453385de6cdb2540e0
bdfprueba43 <117458901+bdfprueba43@users.noreply.github.com>
2022-11-04 08:04:01 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyBs8JTnQrXGFnFYjPugXb0Kx0LeRORVyys",
+  authDomain: "bdfpru.firebaseapp.com",
+  projectId: "bdfpru",
+  storageBucket: "bdfpru.appspot.com",
+  messagingSenderId: "230721641716",
+  appId: "1:230721641716:web:f1cb3eb47bc4d0bba9d2a1",
+  measurementId: "G-PK42NM2092"
 });
