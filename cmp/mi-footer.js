86da1d6ae3e4fccc6b43559d84fc8db29ccbec16
bdfprueba43 <117458901+bdfprueba43@users.noreply.github.com>
2023-07-11 20:58:40 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `<p>
-        &copy; 2022
+        &copy; 2023
         Ricardo Armando Machorro Reyes.
       </p>`;
   }
